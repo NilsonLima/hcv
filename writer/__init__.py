ae33cb54d5f0e6c5881ef809207cdd90fdd50113
Nilson de Lima Júnior <nilson.ljnr@gmail.com>
2019-10-20 09:44:34 -0300

    add function to write .json file

--- writer/__init__.py
+++ writer/__init__.py
@@ -1,5 +1,11 @@
 import pandas as pd
+import json
 
-def write_data_into_xlsx_file(data, columns, index, filename = 'output.xlsx'):
+def write_data_into_xlsx_file(data, columns, index, filename='output.xlsx'):
     pd.DataFrame(data, index=index, columns=columns).to_excel(filename)
+    return
+
+def write_data_into_json_file(data, filename='experiments.json'):
+    with open(filename, 'w') as file:
+        json.dump(data, file, indent=4)
     return
